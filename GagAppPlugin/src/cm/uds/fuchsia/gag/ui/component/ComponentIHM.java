256faf0cc44db47eee82101ab7cc98b3c0936b88
Joskel <joskeltagueu@gmail.com>
2021-08-04 17:42:50 +0100

    UI re-organized

--- GagAppPlugin/src/cm/uds/fuchsia/gag/ui/component/ComponentIHM.java
+++ GagAppPlugin/src/cm/uds/fuchsia/gag/ui/component/ComponentIHM.java
@@ -87,7 +87,7 @@ public class ComponentIHM {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 703, 528);
+		frame.setBounds(100, 100, 900, 528);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		try {
@@ -140,7 +140,7 @@ public class ComponentIHM {
 		panelMetaData.setLayout(new GridLayout(1, 0, 0, 0));
 
 		JPanel panelConfValue = new JPanel();
-		panelMetaData.add(panelConfValue);
+		//panelMetaData.add(panelConfValue); /* old UI */
 		panelConfValue.setLayout(new BorderLayout(0, 0));
 
 		JPanel panelConValueTitle = new JPanel();
@@ -190,6 +190,16 @@ public class ComponentIHM {
 
 		panelConfigurationGraph = new JPanel();
 		panelConfiguration.add(panelConfigurationGraph, BorderLayout.CENTER);
+		
+		// new place for value
+		JPanel panelForConfvalue = new JPanel();
+		panelForConfvalue.setPreferredSize(new Dimension(300,400));
+		panelConfiguration.add(panelForConfvalue,BorderLayout.EAST);
+		panelForConfvalue.setLayout(new BorderLayout());
+
+		panelMetaData.remove(panelForConfvalue);
+		panelForConfvalue.add(panelConfValue,BorderLayout.CENTER);
+		
 	}
 
 	public void disposeTheGraph(GAGAspect gag) {
